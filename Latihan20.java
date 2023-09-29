d86c213b7a4bb808913210212870e02697e41742
Moch. Haikal Putra Muhajir <141520793+Ekallium@users.noreply.github.com>
2023-09-29 14:59:25 +0700

    Delete Latihan20.java

--- Latihan20.java
+++
@@ -1,7 +0,0 @@
-public class Latihan20 {
-
-    public static void main (String[] args) {
-
-        System.out.println("********** IDENTITAS DIRI **********\nNama    : Moch. Haikal Putra Muhajir\nAbsen   : 20\nKelas   : 1A\nJurusan : Teknologi Informasi\nProdi   : Sistem Informasi Bisnis");
-    }
-}
