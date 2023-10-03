1e45989b9a5d74915dabff6f1a50951c278801d1
Moch. Haikal Putra Muhajir <haikalputra1231@gmail.com>
2023-10-03 08:22:40 +0700

    Create Pemilihan2Percobaan119.java

---
+++ JOBSHEET 6/Pemilihan2Percobaan119.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan119 {
+    public static void main(String[] args) {
+        Scanner input19 = new Scanner(System.in);
+        System.out.print("Masukkan Tahun : ");
+        int tahun = input19.nextInt();
+        input19.close();
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0)
+                System.out.println("Tahun Kabisat");
+        } else
+            System.out.println("Bukan Tahun Kabisat");
+
+    }
+}
