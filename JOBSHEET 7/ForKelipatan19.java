fce4e34f0cd4ed57c7f7637ee1f06d7c3e6310d3
Moch. Haikal Putra Muhajir <haikalputra1231@gmail.com>
2023-10-10 08:30:53 +0700

    Create ForKelipatan19.java

---
+++ JOBSHEET 7/ForKelipatan19.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ForKelipatan19{
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int jumlah, counter, kelipatan;
+    jumlah = 0;
+    counter = 0;
+    System.out.print("Masukkan bilangan kelipatan (1-9) : ");
+    kelipatan = scan.nextInt();
+    for(int i = 1; i <= 50; i++){
+        if(i % kelipatan == 0) {
+            jumlah += i;
+            counter++;
+        }
+    } System.out.printf("Banyaknya kelipatan bilangan %d dari 1 - 50 adalah %d\n", kelipatan, counter);
+      System.out.printf("Total bilangan kelipatan %d dari 1 - 50 adalah %d\n", kelipatan, jumlah);
+    
+    }
+}
