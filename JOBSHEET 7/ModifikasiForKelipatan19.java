a0401234282255981edd95fb0ed865b371574e97
Moch. Haikal Putra Muhajir <haikalputra1231@gmail.com>
2023-10-10 08:51:03 +0700

    update jobsheet 7

--- JOBSHEET 7/ModifikasiForKelipatan19.java
+++ JOBSHEET 7/ModifikasiForKelipatan19.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
-public class ForKelipatan19 {
+public class ModifikasiForKelipatan19 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int jumlah, counter, kelipatan;
+        double rata;
         jumlah = 0;
         counter = 0;
         System.out.print("Masukkan bilangan kelipatan (1-9) : ");
@@ -14,8 +15,10 @@ public class ForKelipatan19 {
                 counter++;
             }
         }
+        rata = jumlah / counter;
         System.out.printf("Banyaknya kelipatan bilangan %d dari 1 - 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 - 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1-50 adalah %.2f", kelipatan, rata);
 
     }
 }
