02fdfadce179c306f2a542954564911d6c8c69cc
Moch. Haikal Putra Muhajir <haikalputra1231@gmail.com>
2023-10-10 09:19:04 +0700

    update js 7

--- JOBSHEET 7/ModifikasiWhileGaji19.java
+++ JOBSHEET 7/ModifikasiWhileGaji19.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class WhileGaji19{
+public class ModifikasiWhileGaji19{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int jumlahKaryawan, jumlahJamLembur;
@@ -10,7 +10,7 @@ public class WhileGaji19{
         jumlahKaryawan = scan.nextInt();
         int i = 0;
         while (i < jumlahKaryawan){
-            System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
+            System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan/Pegawai");
             System.out.print("Masukkan jabatan karyawan ke-" + (i+i) + " : ");
             jabatan = scan.next();
             System.out.print("Masukkan jumlah jam lembur : ");
@@ -22,7 +22,7 @@ public class WhileGaji19{
                 continue;
             } else if (jabatan.equalsIgnoreCase("manajer")){
                 gajiLembur = jumlahJamLembur * 100000;
-            } else if (jabatan.equalsIgnoreCase("karyawan")){
+            } else if (jabatan.equalsIgnoreCase("karyawan") || jabatan.equalsIgnoreCase("pegawai")){
                 gajiLembur = jumlahJamLembur * 75000;
             }
             totalGajiLembur += gajiLembur;
