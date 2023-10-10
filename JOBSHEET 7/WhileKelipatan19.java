4036e1c0729b633aa19544d6eed93dee1dc6278b
Moch. Haikal Putra Muhajir <haikalputra1231@gmail.com>
2023-10-10 08:55:43 +0700

    Create WhileKelipatan19.java

---
+++ JOBSHEET 7/WhileKelipatan19.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class WhileKelipatan19 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int jumlah, counter, kelipatan, i = 1;
+        double rata;
+        jumlah = 0;
+        counter = 0;
+        System.out.print("Masukkan bilangan kelipatan (1-9) : ");
+        kelipatan = scan.nextInt();
+        while (i <= 50) {
+            if (i % kelipatan == 0) {
+                jumlah += i;
+                counter++;
+            }
+            i++;
+        }
+        rata = jumlah / counter;
+        System.out.printf("Banyaknya kelipatan bilangan %d dari 1 - 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 - 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1-50 adalah %.2f", kelipatan, rata);
+    }
+}
