54a7eca9f1c8054238cd052123dc41582c71adbe
Moch. Haikal Putra Muhajir <haikalputra1231@gmail.com>
2023-10-10 09:57:13 +0700

    modifikasi do while

---
+++ JOBSHEET 7/ModifikasiDoWhileCuti19.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ModifikasiDoWhileCuti19{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+        System.out.print("jatah cuti : ");
+        jatahCuti = sc.nextInt();
+
+        do {
+            System.out.print("Apakah Anda ingin mengambil cuti (y/t)? : ");
+            konfirmasi = sc.next();
+
+                if (konfirmasi.equalsIgnoreCase("y")) {
+                    System.out.print("Jumlah hari : ");
+                    jumlahHari = sc.nextInt();
+
+                        if(jumlahHari <= jatahCuti) {
+                            jatahCuti -= jumlahHari;
+                            System.out.println("Sisa jatah cuti Anda : " + jatahCuti);
+                        } else {
+                            System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                        }
+                }
+        } while (jatahCuti > 0);
+        sc.close();
+    }
+}
