da9716bdb54663cfcb6ecadb7dd18ae554c36c70
Moch. Haikal Putra Muhajir <haikalputra1231@gmail.com>
2023-10-24 09:51:32 +0700

    Update ArrayRataNilai19.java

--- JOBSHEET 8/ArrayRataNilai19.java
+++ JOBSHEET 8/ArrayRataNilai19.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class ArrayRataNilai19{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukkan jumlah Mahasiswa : ");
+        int jmlMhs = sc.nextInt();
+        int[] nilaiMhs = new int[jmlMhs];
+        double totalLulus = 0;
+        double totalTdkLulus= 0;
+        int lulus = 0;
         for (int i = 0; i < nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) +" : ");
             nilaiMhs[i] = sc.nextInt();
+            if (nilaiMhs [i] > 70){
+                totalLulus += nilaiMhs[i];
+                lulus++;
+            } else
+            totalTdkLulus += nilaiMhs[i];
         } 
-        for (int i = 0; i < nilaiMhs.length; i++){
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
+        System.out.println("Rata-Rata Nilai Lulus = " + totalLulus / lulus);
+        System.out.println("Rata-Rata Nilai Tidak Lulus = " + totalTdkLulus / ( nilaiMhs.length - lulus ));
     } 
 }
