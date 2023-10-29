d7cf09716f43e2f94ae506f8106a5c4b0ade978c
Moch. Haikal Putra Muhajir <haikalputra1231@gmail.com>
2023-10-29 15:04:18 +0700

    Update Array19.java

--- JOBSHEET 9/Array19.java
+++ JOBSHEET 9/Array19.java
@@ -6,20 +6,23 @@ public class Array19{
         System.out.print("Masukkan Jumlah Siswa : ");
         int jmlElemen = sc.nextInt();
         int[] nilai = new int[jmlElemen];
-        int nilaiTinggi = 0;
-        int nilaiRendah = 2147483647;
-        for (int i = 0; i < nilai.length; i++){
+        System.out.print("Masukkan IQ Siswa ke-1 : ");
+        nilai[0] = sc.nextInt();
+        total += nilai[0];
+        int nilaiTinggi = nilai[0];
+        int nilaiRendah = nilai[0];
+        for (int i = 1; i < nilai.length; i++){
             System.out.print("Masukkan IQ Siswa ke-" + (i + 1) + " : ");
             nilai[i] = sc.nextInt();
             total += nilai[i];
-            if (nilai[i] > nilaiTinggi){
+            if (nilai[i] >= nilaiTinggi){
                 nilaiTinggi = nilai[i];
             } 
-            if (nilai[i] < nilaiRendah){
+            if (nilai[i] <= nilaiRendah){
                 nilaiRendah = nilai[i];
             }
         }
-        int avg = total / jmlElemen;
+        double avg =(double)total / jmlElemen;
         System.out.println("IQ Tertinggi Adalah       : " + nilaiTinggi);
         System.out.println("IQ Terendah Adalah        : " + nilaiRendah);
         System.out.println("Rata-Rata IQ Siswa Adalah : " + avg);
