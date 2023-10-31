431d08409674322bbf84b93f42a34215e814bfc8
Moch. Haikal Putra Muhajir <haikalputra1231@gmail.com>
2023-10-31 09:20:34 +0700

    for each

--- JOBSHEET 10/Bioskop19.java
+++ JOBSHEET 10/Bioskop19.java
@@ -6,8 +6,8 @@ public class Bioskop19 {
         penonton[2][0] = "Eka";     penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";   penonton[3][1] = "Hana";
         System.out.println("Banyaknya baris : " + penonton.length);
-        for( int i = 0; i < penonton.length; i++){
-            System.out.println("Panjang baris ke-" + (i+1) + " : " + penonton[i].length);
+        for (String[] baris : penonton) {
+            System.out.println("Panjang baris: " + baris.length);
         }
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
